d2c2a130df3a63082ff55ddd29236dc4849c10f7
Vipanpreet Kaur <sohivipan@gmail.com>
2025-04-09 17:17:33 -0700

    Finished

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -10,19 +10,22 @@ function ItemModal({ activeModal, onClose, card, onDelete }) {
           className="modal__close modal__close-preview"
         ></button>
         <img src={card.imageUrl} alt={card.name} className="modal__image" />
-        <div className="modal__footer-caption">
-          <div>
+        <div className="modal__footer">
+          <div className="modal__footer-caption">
             <p className="modal__caption">{card.name}</p>
             <p className="modal__weather">Weather: {card.weather}</p>
           </div>
-          <button
-            onClick={() => onDelete(card)}
-            type="button"
-            className="modal__delete-btn"
-          >
-            {" "}
-            Delete Item
-          </button>
+
+          <div className="modal__delete">
+            <button
+              onClick={() => onDelete(card)}
+              type="button"
+              className="modal__delete-btn"
+            >
+              {" "}
+              Delete Item
+            </button>
+          </div>
         </div>
       </div>
     </div>
